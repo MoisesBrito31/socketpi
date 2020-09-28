6fe0a6c145ea9d966c9406eaa56d2790cbf2ccb7
MoisesBrito31 <moisesbritofuentes@gmail.com>
2020-09-28 09:09:51 -0300

    criação da base do protocolo

--- protocolo/protocolo.py
+++ protocolo/protocolo.py
@@ -1,10 +1,32 @@
 import socket
 
-HEADERSIZE = 10
-IP = "192.168.0.100"
-PORT = 8844
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+class Protocolo():
+    HEADERSIZE = 512
+    PORT = 8844
+    ip = "192.168.0.100"
+    trans = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    def __init__(self, _ip):
+        self.ip = _ip
+
+    def __comandoSimples(self,str:cmd,int:format):
+        ret = "falha"
+        try:
+            self.trans.connect((self.ip,self.PORT))
+            msg = bytes(cmd,"utf-8")
+            resposta_bytes = self.trans.recv(self.HEADERSIZE)
+            resposta = resposta_bytes.decode('utf-8')
+            
+            pass
+        except Exception as e:
+            pass
+        return ret
+"""
+
+
+
+
 s.connect((IP, PORT))
 
 msg = "CMD0001,1,5,199,0,0"
@@ -12,3 +34,4 @@ s.send(bytes(msg,"utf-8"))
 
 r_msg = s.recv(512)
 print(f"{r_msg.decode('utf-8')}")
+"""
